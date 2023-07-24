c501d1a156ccd85aedea57c2a1010ebcba95f732
Gokul <gokulgamer9404@gmail.com>
2023-07-24 19:22:00 +0530

    Object detection basic

---
+++ Object Tracking/object_tracking.py
@@ -0,0 +1,10 @@
+"""
+What is Object Tracking?
+Object tracking is a Computer Vision technique for locating postion of object in the image or videos    
+
+Tyeps of Object Tracking:-
+    -> Frame Differencing
+    -> Color Segmentation
+    -> Feature Matching
+    -> Machine Learning based
+"""
